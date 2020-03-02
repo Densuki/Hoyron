7b632883b2504d28bd2e181f4dc6f04d3ba4c3e8
Densuki <DarkShadow3315@gmail.com>
2020-03-01 22:21:45 -0300

    Correção

--- commands/moderation/prune.js
+++ commands/moderation/prune.js
@@ -6,10 +6,11 @@ module.exports = {
     category: "moderation",
     description: "um comando que permite excluir as mensagens",
     usage: "<input> + number",
-    run: (client, message, args) => {	
+    run: (client, message, args) => {
+	message.delete();		
   //if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Você não pode fazer isso! Você não possui permissões para executar esse comando!");
   if (!message.member.roles.some(r=>["Linked", "STAFF", "Staff"].includes(r.name))) return message.reply("Você não pode fazer isso! Você não possui permissões para executar esse comando!");
-  if(!args[0]) return message.channel.send("Nain!\n Esqueceu de adicionar um número!").then(msg => msg.delete(10000));});
+  if(!args[0]) return message.channel.send("Nain!\n Esqueceu de adicionar um número!").then(msg => msg.delete(10000));
   message.channel.bulkDelete(args[0]).then(() => {
   message.channel.send(`Mensagens ${args[0]} foram apagadas.`).then(msg => msg.delete(300));});
   //message.channel.send(``).then(msg => msg.delete(60));
