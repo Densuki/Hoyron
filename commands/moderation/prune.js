54fc7f2597b92a31c1ab266b76314e2e8bae2261
Densuki <DarkShadow3315@gmail.com>
2020-03-01 22:12:21 -0300

    Novo comando - Purge
    Purge, prune, clear, apagar... Tanto faz, ele só apaga :v

---
+++ commands/moderation/prune.js
@@ -0,0 +1,18 @@
+const { RichEmbed } = require("discord.js");
+
+module.exports = {
+    name: "purge",
+    aliases: ["prune", "clear", "apagar"],
+    category: "moderation",
+    description: "um comando que permite excluir as mensagens",
+    usage: "<input> + number",
+    run: (client, message, args) => {
+  //if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("Você não pode fazer isso! Você não possui permissões para executar esse comando!");
+  //if (!message.member.roles.some(r=>["STAFF"].includes(r.name))) return message.reply("Você não pode fazer isso! Você não possui permissões para executar esse comando!");
+  if (!message.member.roles.some(r=>["Linked", "STAFF", "Staff"].includes(r.name))) return message.reply("Você não pode fazer isso! Você não possui permissões para executar esse comando!");
+  if(!args[0]) return message.channel.send("Nain!\n Esqueceu de adicionar um número!");
+  message.channel.bulkDelete(args[0]).then(() => {
+  message.channel.send(`Mensagens ${args[0]} foram apagadas.`).then(msg => msg.delete(300));});
+  //message.channel.send(``).then(msg => msg.delete(60));
+   }  
+}
