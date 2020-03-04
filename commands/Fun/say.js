6982124b7576f17020c17e16bc080418247831be
Densuki <DarkShadow3315@gmail.com>
2020-03-03 21:57:35 -0300

    Atualização no comando "Say"
    Remoção da foto de Hoyron

--- commands/Fun/say.js
+++ commands/Fun/say.js
@@ -5,19 +5,20 @@ module.exports = {
     aliases: ["bc", "broadcast", "falar"],
     category: "moderation",
     description: "você fala através do bot (versão Embed)",
-    usage: "<input> <mensagem> ou <input> embed <mensagem>",
+    usage: "<input> + mensagem ou <input> [embed] + mensagem",
     run: (client, message, args) => {
         // Check if you can delete the message
         message.delete();
-
-        if (!message.member.hasPermission("MANAGE_MESSAGES"))
+        //let Linked = message.guild.roles.get("638081258487939072");
+        //if (!message.member.hasPermission("MANAGE_MESSAGES"))
+        if (!message.member.roles.some(r=>["Linked"].includes(r.name)) ) 
             return message.reply("Você não tem as permissões necessárias para usar este comando.").then(m => m.delete(5000));
-
-        if (args.length < 0)
-            return message.reply("Nada a dizer?").then(m => m.delete(5000));
+        if(!args[0]) return message.channel.send("Nada a dizer?").then(m => m.delete(5000));
+        //if (args.length < 0)
+        //    return message.reply("Nada a dizer?").then(m => m.delete(5000));
 
     // Role color
-    const roleColor = message.guild.me.highestRole.hexColor;
+     const roleColor = message.guild.me.highestRole.hexColor;
 
     // If the first argument is embed, send an embed,
     // otherwise, send a normal message
@@ -25,13 +26,13 @@ module.exports = {
         const embed = new RichEmbed()
             .setDescription(args.slice(1).join(" "))
             .setColor(roleColor === "#000000" ? "#ffffff" :  roleColor)
-            .setTimestamp()
-            .setImage(client.user.displayAvatarURL)
-            .setAuthor(message.author.username, message.author.displayAvatarURL);
+            //.setTimestamp()
+            //.setImage(client.user.displayAvatarURL)
+            //.setAuthor(message.author.username, message.author.displayAvatarURL);
 
-        message.channel.send(embed);
-    } else {
-        message.channel.send(args.join(" "));
-        }
-    }
+         message.channel.send(embed);
+     } else {
+         message.channel.send(args.join(" "));
+         }
+     }
 }
